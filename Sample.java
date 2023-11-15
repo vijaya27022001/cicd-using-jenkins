d30b3dfd8add04181ebe4207c21106c68a6004dd
vijaya27022001 <kolisettivijayalakshmi@gmail.com>
2023-11-15 10:58:30 +0530

    Initial commit

---
+++ Sample.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+public class Sample{
+    public static void main(String args[]){
+         System.out.println("This is Sample Java Program");
+         System.out.println("Output from Java Program");
+         System.out.println(new Date());
+
+    }
+}
